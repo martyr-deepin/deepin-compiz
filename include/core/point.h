ad8815f675bc25ed622a2153e3ecfb03712cf325
hk <hukang@linuxdeepin.com>
2013-05-22 15:05:23 +0800

    include/core/point.h: retain CompPoint::vector definition

--- include/core/point.h
+++ include/core/point.h
@@ -26,6 +26,9 @@
 #ifndef _COMPPOINT_H
 #define _COMPPOINT_H
 
+#include <vector>
+#include <list>
+
 /**
  * A 2D coordinate (likely in screen space) that can only be mutated
  * through set() methods, since it's data members are private.
@@ -81,6 +84,11 @@ class CompPoint {
 	 * Returns a subtracted point
 	 */
 	CompPoint operator- (const CompPoint &) const;
+
+	typedef std::vector<CompPoint> vector;
+	typedef std::vector<CompPoint *> ptrVector;
+	typedef std::list<CompPoint> list;
+	typedef std::list<CompPoint *> ptrList;
 
     private:
 	int mX, mY;
